bc8547343ec83c137a647ec09dfe7172dc1d959a
qiulin <3260296133@qq.com>
2025-04-03 18:37:58 +0800

    [add] dns_parser

--- dns_parser.h
+++ dns_parser.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "protocol_layer.h"
+#include <string>
+#include <vector>
+
+class DNSParser : public ProtocolLayer {
+public:
+    DNSParser(const uint8_t* data, size_t length);
+
+    ProtocolType type() const noexcept override;
+
+    std::string summary() const noexcept override;
+
+    const uint8_t* payload() const noexcept override;
+
+    size_t payload_length() const noexcept override;
+
+    std::string get_domain_name() const;
+
+private:
+    void parse();
+
+    std::string domain_name_;
+};
